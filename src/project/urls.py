f0946127c0f35c3b26bceebd761710c86c0b2858
Yuriy Yakovlev <yyyakovlev@gmail.com>
2020-03-30 20:24:49 +0300

    jpg3

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,4 +1,4 @@
-rom pathlib import Path
+from pathlib import Path
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
@@ -11,13 +11,24 @@ def view(r):
         return HttpResponse(f.read())
 
 def jpg(r):
-    jpg = here.parent.parent / "pic/img-7655.jpg"
+    jpg = here.parent.parent / "src/pic/img-7655.jpg"
     with jpg.open('rb') as f:
-        return HttpResponse(f.read(), content_type="img/jpg")
+        return HttpResponse(f.read(), content_type="img/jpeg")
 
+def jpg2(r):
+    jpg2 = here.parent.parent / "src/pic/computer-icons.jpg"
+    with jpg2.open('rb') as f:
+        return HttpResponse(f.read(), content_type="img/jpeg")
+
+def jpg3(r):
+    jpg3 = here.parent.parent / "src/pic/ico-icon-ghost.jpg"
+    with jpg3.open('rb') as f:
+        return HttpResponse(f.read(), content_type="img/jpeg")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jpg', view.jpg),
+    path('jpg/', jpg),
+    path('jpg2/', jpg2),
+    path('jpg3/', jpg3),
     path('', view),
 ]
