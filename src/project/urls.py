bb3d7b67a7bc35ff1a8bab5cf441d6c161f7366c
Yuriy Yakovlev <yyyakovlev@gmail.com>
2020-03-30 21:48:45 +0300

    jpg3

--- src/project/urls.py
+++ src/project/urls.py
@@ -5,30 +5,30 @@ from django.urls import path
 
 here = Path(__file__).parent.resolve()
 
-def view(r):
+def view():
     index = here.parent.parent / "index.html"
     with index.open() as f:
         return HttpResponse(f.read())
 
-def jpg(r):
+def jpg():
     jpg = here.parent.parent / "src/pic/IMG-7655.jpg"
     with jpg.open('rb') as f:
         return HttpResponse(f.read(), content_type="img/jpeg")
 
-def jpg2(r):
+def jpg2():
     jpg2 = here.parent.parent / "src/pic/computer-icons.jpg"
     with jpg2.open('rb') as f:
         return HttpResponse(f.read(), content_type="img/jpeg")
 
-def jpg3(r):
+def jpg3():
     jpg3 = here.parent.parent / "src/pic/ico-icon-ghost.jpg"
     with jpg3.open('rb') as f:
         return HttpResponse(f.read(), content_type="img/jpeg")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', view),
     path('jpg/', jpg),
     path('jpg2/', jpg2),
     path('jpg3/', jpg3),
-    path('', view)
 ]
