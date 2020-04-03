471484ae5e12dcc7795bf718dab8bb14b38d8a77
Yuriy Yakovlev <yyyakovlev@gmail.com>
2020-04-03 19:13:38 +0300

    improvements

--- src/project/urls.py
+++ src/project/urls.py
@@ -5,30 +5,51 @@ from django.urls import path
 
 here = Path(__file__).parent.resolve()
 
-def view():
-    index = here.parent.parent / "index.html"
-    with index.open() as f:
-        return HttpResponse(f.read())
+def real_static (file, content_type):
+    with file.open('rb') as f:
+        return HttpResponse(f.read(), content_type)
 
-def jpg():
-    jpg = here.parent.parent / "src/pic/IMG-7655.jpg"
-    with jpg.open('rb') as f:
-        return HttpResponse(f.read(), content_type="img/jpeg")
+def view(r):
+    return real_static(here.parent.parent / "index.html", None)
 
-def jpg2():
-    jpg2 = here.parent.parent / "src/pic/computer-icons.jpg"
-    with jpg2.open('rb') as f:
-        return HttpResponse(f.read(), content_type="img/jpeg")
+    # index = here.parent.parent / "index.html"
+    # with index.open() as f:
+    #     return HttpResponse(f.read())
 
-def jpg3():
-    jpg3 = here.parent.parent / "src/pic/ico-icon-ghost.jpg"
-    with jpg3.open('rb') as f:
-        return HttpResponse(f.read(), content_type="img/jpeg")
+def jpg(r):
+    return real_static(here.parent.parent / "src/pic/IMG-7655.jpg", "img/jpeg")
+
+    # jpg = here.parent.parent / "src/pic/IMG-7655.jpg"
+    # with jpg.open('rb') as f:
+    #     return HttpResponse(f.read(), content_type="img/jpeg")
+
+def jpg2(r):
+    return real_static(here.parent.parent / "src/pic/computer-icons.jpg", "img/jpeg")
+
+    # jpg2 = here.parent.parent / "src/pic/computer-icons.jpg"
+    # with jpg2.open('rb') as f:
+    #     return HttpResponse(f.read(), content_type="img/jpeg")
+
+def jpg3(r):
+    return real_static( here.parent.parent / "src/pic/ico-icon-ghost.jpg", "img/jpeg")
+
+    # here.parent.parent / "src/pic/computer-icons.jpg"
+    # jpg3 = here.parent.parent / "src/pic/ico-icon-ghost.jpg"
+    # with jpg3.open('rb') as f:
+    #     return HttpResponse(f.read(), content_type="img/jpeg")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', view),
     path('jpg/', jpg),
     path('jpg2/', jpg2),
     path('jpg3/', jpg3),
+    path('', view),
 ]
+
+# def view1(r):
+# открываем ФАЙЛ1
+# читаем содержимое
+# создаём HttpResponse
+# с содержимым файла
+# и ТИПОМ КОНТЕНТА1
+# возвращаем из функции этот респонс
