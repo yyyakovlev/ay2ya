9ba8513dc493e7790313fe776bb33e8d8ac845e7
Yuriy Yakovlev <yyyakovlev@gmail.com>
2020-04-06 16:28:15 +0300

    new page

--- src/project/urls.py
+++ src/project/urls.py
@@ -2,48 +2,72 @@ from pathlib import Path
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
+from django.shortcuts import render
 
 here = Path(__file__).parent.resolve()
 
-def real_static (file, content_type):
-    with file.open('rb') as f:
+def r_static (fn, content_type):
+    with fn.open('rb') as f:
         return HttpResponse(f.read(), content_type)
 
-def view(r):
-    return real_static(here.parent.parent / "index.html", None)
+# def view(r):
+#     return r_static(here.parent.parent / "PROJECT_DIR/templates/index.html", None)
+
+def view(req):
+    return render(req, "index.html")
+
+def view_cv(req):
+    return render(req, "cv.html")
+
+def view_prj(req):
+    return render(req, "projects.html")
+#
+# def view_ct(req):
+#     return render(req, "contacts.html")
 
     # index = here.parent.parent / "index.html"
     # with index.open() as f:
     #     return HttpResponse(f.read())
 
+def dj(r):
+    return r_static( here.parent.parent / "src/pic/dj.png", "img/jpeg")
+
 def jpg(r):
-    return real_static(here.parent.parent / "src/pic/IMG-7655.jpg", "img/jpeg")
+    return r_static(here.parent.parent / "src/pic/IMG-7655.jpg", "img/jpeg")
 
     # jpg = here.parent.parent / "src/pic/IMG-7655.jpg"
     # with jpg.open('rb') as f:
     #     return HttpResponse(f.read(), content_type="img/jpeg")
 
-def jpg2(r):
-    return real_static(here.parent.parent / "src/pic/computer-icons.jpg", "img/jpeg")
+def gmail(r):
+    return r_static(here.parent.parent / "src/pic/gmail.png", "img/jpeg")
 
-    # jpg2 = here.parent.parent / "src/pic/computer-icons.jpg"
-    # with jpg2.open('rb') as f:
-    #     return HttpResponse(f.read(), content_type="img/jpeg")
 
-def jpg3(r):
-    return real_static( here.parent.parent / "src/pic/ico-icon-ghost.jpg", "img/jpeg")
+def telega(r):
+    return r_static( here.parent.parent / "src/pic/telegram.png", "img/jpeg")
 
-    # here.parent.parent / "src/pic/computer-icons.jpg"
-    # jpg3 = here.parent.parent / "src/pic/ico-icon-ghost.jpg"
-    # with jpg3.open('rb') as f:
-    #     return HttpResponse(f.read(), content_type="img/jpeg")
+def body_bg(r):
+    return r_static( here.parent.parent / "src/pic/body.jpg", "img/jpeg")
+
+def header_bg(r):
+    return r_static( here.parent.parent / "src/pic/header.jpg", "img/jpeg")
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('jpg/', jpg),
-    path('jpg2/', jpg2),
-    path('jpg3/', jpg3),
+    path('dj/', dj),
+    path('gmail/', gmail),
+    path('telega/', telega),
+    path('body_bg/', body_bg),
+    path('header_bg/', header_bg),
     path('', view),
+    path('indx/', view),
+    path('view_cv/', view_cv),
+    path('view_prj/', view_prj),
+    # path('view_ct/', view_ct),
 ]
 
 # def view1(r):
