f0ea2aa7872b3360a2fa711a82df96968db2f664
Yuriy Yakovlev <yyyakovlev@gmail.com>
2020-04-06 20:02:41 +0300

    + static

--- src/project/settings.py
+++ src/project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -111,4 +112,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+
+STATICFILES_DIRS = [
+    PROJECT_DIR / 'static',
+]
+
+STATIC_ROOT = REPO_DIR / ".static"
