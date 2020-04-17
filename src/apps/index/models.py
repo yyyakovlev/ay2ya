9493acfb702c2c7e01acf09da1d8040e4f1e79d1
Yuriy Yakovlev <yyyakovlev@gmail.com>
2020-04-17 21:42:27 +0300

    models

--- src/apps/index/models.py
+++ src/apps/index/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django.db import models as m
+
+class UserInfo(m.Model):
+    name = m.TextField(unique=True)
+    grts = m.TextField(null=True, blank=True)
+    age = m.IntegerField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "User Info"
+
+    # Отображение текста в админке
+    def __str__(self):
+        return f"UserInfo(id={self.pk}, name={self.name!r})"
+
+
+
+
+
+
+# a = UserInfo()
+#
+# print(a.name)
+
 
 # Create your models here.
