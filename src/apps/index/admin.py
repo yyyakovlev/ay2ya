16c823269eae38e2ab22b36fec0e38baadd54604
Yuriy Y <62623563+yyyakovlev@users.noreply.github.com>
2020-04-17 22:05:30 +0300

    models (#1)

--- src/apps/index/admin.py
+++ src/apps/index/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from apps.index.models import UserInfo
 
 # Register your models here.
+
+
+@admin.register(UserInfo)
+class UserInfoAdmMod(ModelAdmin):
+    pass
+
