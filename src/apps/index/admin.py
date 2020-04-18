57c7d7e485f6909a4cf5b61e7e83bac0a8ff2fe2
Yuriy Yakovlev <yyyakovlev@gmail.com>
2020-04-18 14:28:15 +0300

    age added 2 models & repaired

--- src/apps/index/admin.py
+++ src/apps/index/admin.py
@@ -1,10 +1,14 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
 from apps.index.models import UserInfo
 
-# Register your models here.
-
+class UserInfoAdminForm(forms.ModelForm):
+    class Meta:
+        model = UserInfo
+        fields = "__all__"
+        widgets = {"name": forms.TextInput()}
 
 @admin.register(UserInfo)
 class UserInfoAdmMod(ModelAdmin):
